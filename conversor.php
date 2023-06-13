b5e69666f922768a0ca3213085fbef40c557cf95
Kelvin <kelabrantes.dev@gmail.com>
2023-06-13 10:22:45 -0300

    second commit

--- conversor.php
+++ conversor.php
@@ -9,9 +9,10 @@
 <body>
 
 <?php 
+    $valor = $_GET["valor"];
     $curl = curl_init();
-    $hoje = date("'m/d/Y'");
-    $urlAPI = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao='. "$hoje" .'&$top=100&$format=json';
+    $hoje = date("'m-d-Y'");
+    $urlAPI = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=\'06-09-2023\'&@dataFinalCotacao='.$hoje.'&$top=100&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,cotacaoVenda,dataHoraCotacao';
     $valoresCotacao = [];
 
     curl_setopt_array($curl, [
@@ -34,14 +35,19 @@
     $format_Date = explode("-", $dataHoraCotacao);
     $format_Date_hora = explode(" ", $format_Date[2]);
 
+    $resultado = $valor/$cotacaoCompra;
+    $resultado_final = number_format($resultado, 2, '.')
+
 ?>
 
 
     <div class="block-cotacao">
-        <p><strong>Cotação-Venda:</strong> <?php echo "$cotacaoVenda"?></p>
-        <p><strong>Cotação-Compra:</strong> <?php echo "$cotacaoCompra"?></p>
+        <p><strong>Cotação-Venda:</strong> <?php print_r(number_format($cotacaoCompra, 2, '.'))?></p>
+        <p><strong>Cotação-Compra:</strong> <?php  print_r(number_format($cotacaoVenda, 2, '.'))?></p>
         <p><strong>Data e Hora/Cotação:</strong> <?php print_r($format_Date_hora[0]."/".$format_Date[1]."/".$format_Date[0]." - ".$format_Date_hora[1])?></p>
+        <h3>Valor da consulta - Real para Dolar <?php echo "$resultado_final" ?></h3>
     </div>
+        
 
     <br>
 
@@ -50,6 +56,7 @@
 
 </body>
 </html>
+
 
 
 
